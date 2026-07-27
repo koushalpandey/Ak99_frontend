f9a153f5cd9ad0aac6127d74b5efad2be9d5c60a
koushal-dev <koushal9676@gmail.com>
2026-07-27 14:16:34 +0530

    Refactor routes structure for improved readability and consistency

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,42 +1,43 @@
 import { createBrowserRouter } from "react-router-dom";
+
 import MainLayout from "../layout/mainLayout";
 import Homepage from "../pages/HomepagePages/homepage";
-import ProductDetailsPage from "../pages/PoductDetailPages/productDetailsPage"
+import ProductDetailsPage from "../pages/PoductDetailPages/productDetailsPage";
 import Login from "../auth/authpages/loginPage";
 import CheckoutPage from "../pages/otherPages/checkout";
 import WishlistComponent from "../pages/otherPages/wishlistPage";
 import CategoriesProductPage from "../pages/otherPages/categorieProduct";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <MainLayout />,
-        children: [
-            {
-                index: true,
-                element: <Homepage />,
-            },
-            {
-                path:"/login",
-                element: <Login />,
-            },
-            {
-                path:"/productDetail/:slug",
-                element: <ProductDetailsPage />,
-            },
-            {
-                path:"/checkout",
-                element: <CheckoutPage />,
-            },
-            {
-                path:"/wishlist",
-                element: <WishlistComponent />,
-            },
-            {
-                path:"/categorie/:slug",
-                element: <CategoriesProductPage />,
-            },
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+      {
+        path: "productDetail/:slug",
+        element: <ProductDetailsPage />,
+      },
+      {
+        path: "checkout",
+        element: <CheckoutPage />,
+      },
+      {
+        path: "wishlist",
+        element: <WishlistComponent />,
+      },
+      {
+        path: "categorie/:slug",
+        element: <CategoriesProductPage />,
+      },
+    ],
+  },
 
-        ],
-    },
+  {
+    path: "/login",
+    element: <Login />,
+  },
 ]);
