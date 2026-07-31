a2b791b95afa83cbe9be330dbdf797a9324cfafc
koushal-dev <koushal9676@gmail.com>
2026-07-31 18:27:38 +0530

    Remove LocationProvider component and its usage in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes";
-import LocationProvider from "./components/useFullcomponents/locationProvider";
+
 
 function App() {
   return (
     <>
-      <LocationProvider />
+
       <RouterProvider router={router} />
     </>
   )
